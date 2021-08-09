5df1d1f81906faa3a39dc59013346ca970e1ad8b
ebinezh <88050562+ebinezh@users.noreply.github.com>
2021-08-09 21:53:45 +0530

    Add files via upload

---
+++ simple number pattern.py
@@ -0,0 +1,11 @@
+# simple number triangular pattern
+rows = int(input("Enter value: "))
+rows = rows+1
+for num in range(rows):
+    for i in range(num):
+        print(num, end=" ")
+    print(" ")
+
+
+    #dream2code
+
